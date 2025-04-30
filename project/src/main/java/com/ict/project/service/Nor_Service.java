54c1f1dc63d830d0c8589ad039163dfc27c73714
Wlrajr <149646638+Wlrajr@users.noreply.github.com>
2025-04-30 16:27:58 +0900

    Delete project directory

--- project/src/main/java/com/ict/project/service/Nor_Service.java
+++
@@ -1,9 +0,0 @@
-package com.ict.project.service;
-
-import com.ict.project.vo.personnel.UserVO;
-
-public interface Nor_Service {
-
-    UserVO login(String empEmail, String empPassword);
-
-}
--- project/src/main/java/com/ict/project/service/Store_ServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.ict.project.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class Store_ServiceImpl implements Store_Service{
-
-}
