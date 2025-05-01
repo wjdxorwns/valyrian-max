67a60eba1b46ecb02d65946b36739890858a0189
Wlrajr <149646638+Wlrajr@users.noreply.github.com>
2025-05-01 12:55:53 +0900

    Add files via upload

--- main/java/com/ict/project/service/NorService.java
+++ main/java/com/ict/project/service/NorService.java
@@ -1,5 +1,12 @@
 package com.ict.project.service;
 
-public interface NorService {
+import com.ict.project.vo.personnel.UsersVO;
+import com.ict.project.vo.personnel.EmployeeVO;
 
+public interface NorService {
+    // 이메일로 사용자 조회
+    UsersVO getUserByEmail(String emp_email);
+
+    // user_idx로 직원 조회
+    EmployeeVO getEmployeeByUserIdx(int userIdx);
 }
