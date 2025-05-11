0fe8e3ecd52263eb6337ea6303607dcc4f66a523
Wlrajr <149646638+Wlrajr@users.noreply.github.com>
2025-05-11 15:21:49 +0900

    Delete java/com/ict/project directory

--- java/com/ict/project/service/EmployeeService.java
+++
@@ -1,8 +0,0 @@
-package com.ict.project.service;
-
-import com.ict.project.vo.personnel.EmployeeVO;
-
-public interface EmployeeService {
-    EmployeeVO findByUserIdx(int user_idx);
-    EmployeeVO findByEmpIdx(String emp_idx);
-}
