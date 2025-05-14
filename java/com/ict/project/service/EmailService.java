5eec27f7e588e736eda7b2219b6996afe5254248
Wlrajr <149646638+Wlrajr@users.noreply.github.com>
2025-05-14 20:29:48 +0900

    Delete java/com/ict/project directory

--- java/com/ict/project/service/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.ict.project.service;
-
-public interface EmailService {
-    String createCode();
-    void sendHtmlMail(String toEmail, String subject, String htmlContent) throws Exception;
-    String sendAuthMail(String toEmail) throws Exception;
-    boolean isEmailRegistered(String email); // 이미 정의되어 있음, 오타 수정
-}
